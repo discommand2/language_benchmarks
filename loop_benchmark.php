04cd2392a03096852ac7095295bc7cc96dd6ed6a
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:30:25 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -4,12 +4,11 @@ $cpuCores = shell_exec("nproc"); // Get the number of CPU cores
 $runtime = new \parallel\Runtime();
 $futures = [];
 
-for ($i = 0; $i < $cpuCores; $i++) {
-    $futures[] = $runtime->run(function () {
-        $count_loops = 0;
-        while ($count_loops < 1000000000) ++$count_loops;
-        return $count_loops;
-    });
+for ($i = 0; $i < 8; $i++) {
+    $futures[] = $runtime->run(function ($i) {
+        while (true) echo ($i);
+        return 1;
+    }, [$i]);
 }
 $total_count = 0;
 foreach ($futures as $future) $total_count += $future->value();
