8e43e768b8112efaa06ec161c9b1ada4553317b0
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:52:02 -0500

    Update loop_benchmark.py

--- loop_benchmark.py
+++ loop_benchmark.py
@@ -1,19 +1,13 @@
-import time
-import platform
+import signal
+import sys
 
-# Start the timer
-start_time = time.time()
-end_time = start_time + 10  # 10 seconds later
-
-# Counter for the number of loop iterations
 count_loops = 0
 
-# Run the while loop as many times as possible within 10 seconds
-while time.time() < end_time:
-    count_loops += 1
+def handler(signum, frame):
+    print(f"Python {sys.version_info.major}.{sys.version_info.minor} executed the loop {count_loops:,} times before termination.")
+    sys.exit(0)
 
-# Output the number of loop iterations performed
-per_second = count_loops / 10
-count_loops_formatted = "{:,}".format(count_loops)
-per_second_formatted = "{:,.1f}".format(per_second)
-print(f"Python {platform.python_version()} executed the loop {count_loops_formatted} times in 10 seconds. ({per_second_formatted}/sec)")
+signal.signal(signal.SIGTERM, handler)
+
+while True:
+    count_loops += 1
