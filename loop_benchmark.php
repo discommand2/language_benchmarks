085ddf9e9bed67f7c0945bdc756cbfbd8115f54e
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:05:02 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -1,45 +1,21 @@
 <?php
+// called by: 'timeout 10s php loop_benchmark.php'
+$cpuCores = shell_exec("nproc"); // Get the number of CPU cores
+$runtime = new \parallel\Runtime();
 
-use parallel\{Runtime, Future, Channel, Events};
-
-$numCpus = shell_exec("nproc");
-$channel = Channel::make('loops');
-$stop = false;
-
-$ctrlcHandler = function ($signo) use ($channel, &$stop, &$runtimes) {
-    global $stop;
-    $stop = true;
-    $totalLoops = 0;
-    while (($loops = $channel->recv()) !== null) {
-        $totalLoops += $loops;
-    }
-    echo "PHP " . phpversion() . " looped $totalLoops times.\n";
-    foreach ($runtimes as $runtime) {
-        $runtime->kill();
-    }
-    exit(0);
-};
-
-pcntl_signal(SIGTERM, $ctrlcHandler);
-
-$runtimes = [];
-for ($i = 0; $i < $numCpus; $i++) {
-    $runtimes[] = new Runtime();
-
-    $runtimes[$i]->run(function ($channel) {
-        global $stop;
-        while (true) {
-            for ($j = 0; $j < 1000000; $j++) {
-                // This loop will run a million times before moving on
-            }
-            if ($stop) {
-                break;
-            }
-            $channel->send(1000000);
-        }
-    }, [$channel]);
+$futures = [];
+for ($i = 0; $i < $cpuCores; $i++) {
+    $futures[] = $runtime->run(function () {
+        $count_loops = 0;
+        pcntl_async_signals(true);
+        pcntl_signal(SIGTERM, function () use (&$count_loops) {
+            die("PHP " . phpversion() . " looped " . number_format($count_loops) . " times.\n");
+        });
+        while (true) ++$count_loops;
+    });
 }
 
-while (true) {
-    sleep(1);
+// Retrieve the results when ready
+foreach ($futures as $future) {
+    $result = $future->value();
 }
