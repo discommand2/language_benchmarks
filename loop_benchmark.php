b08bbcf188d1aad876e506f549b24845cb62739a
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 16:54:44 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -1,8 +1,37 @@
 <?php
-// called by: 'timeout 10s php loop_benchmark.php'
-$count_loops = 0;
-pcntl_async_signals(true);
-pcntl_signal(SIGTERM, function () use (&$count_loops) {
-    die("PHP " . phpversion() . " incremented " . number_format($count_loops) . " times.\n");
-});
-while (true) ++$count_loops;
+
+use parallel\{Runtime, Future, Channel};
+
+$numCpus = shell_exec("nproc");
+$channel = Channel::make('loops');
+
+$ctrlcHandler = function ($signo) use ($channel) {
+    $totalLoops = 0;
+    while ($channel->recv()) {
+        $totalLoops += 1;
+    }
+    echo "PHP looped $totalLoops times.\n";
+    exit(0);
+};
+
+pcntl_signal(SIGTERM, $ctrlcHandler);
+
+$runtimes = [];
+for ($i = 0; $i < $numCpus; $i++) {
+    $runtimes[] = new Runtime();
+
+    $runtimes[$i]->run(function ($channel) {
+        while (true) {
+            for ($j = 0; $j < 1000000; $j++) {
+                // This loop will run a million times before moving on
+            }
+            $channel->send(1000000);
+        }
+    }, [$channel]);
+}
+
+foreach ($runtimes as $runtime) {
+    $runtime->close();
+}
+
+pcntl_signal_dispatch();
