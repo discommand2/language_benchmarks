aec0901809045733ea2e19755e241783b9a0ab3b
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 12:19:30 -0500

    rust

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,32 +1,19 @@
-use std::time::{Duration, Instant};
+use std::sync::atomic::{AtomicUsize, Ordering};
+use std::sync::Arc;
+use ctrlc::set_handler;
 
 fn main() {
-    let start = Instant::now();
-    let runtime = Duration::new(10, 0); // 10 seconds
-    let mut count_loops = 0;
+    let count_loops = Arc::new(AtomicUsize::new(0));
+    let count_loops_clone = Arc::clone(&count_loops);
 
-    while Instant::now().duration_since(start) < runtime {
-        count_loops += 1;
+    set_handler(move || {
+        println!("Rust {} executed the loop {} times before termination.",
+                 std::env::consts::RUST_VERSION,
+                 count_loops_clone.load(Ordering::Relaxed));
+        std::process::exit(0);
+    }).expect("Error setting Ctrl-C handler");
+
+    loop {
+        count_loops.fetch_add(1, Ordering::Relaxed);
     }
-
-    let per_second = count_loops / 10;
-    println!("Rust executed the loop {} times in 10 seconds. ({}/sec)",
-             format_with_commas(count_loops),
-             format_with_commas(per_second));
-}
-
-fn format_with_commas(num: u64) -> String {
-    let num_str = num.to_string();
-    let mut result = String::new();
-    
-    // Reverse the string and insert commas every three digits
-    for (i, ch) in num_str.chars().rev().enumerate() {
-        if i % 3 == 0 && i != 0 {
-            result.push(',');
-        }
-        result.push(ch);
-    }
-    
-    // The string is reversed at this point, so reverse again to correct it
-    result.chars().rev().collect()
 }
