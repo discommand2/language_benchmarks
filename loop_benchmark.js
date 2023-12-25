81b097aa73cda8e2d9970037fe1f61197c16d41f
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:34:10 -0500

    Update loop_benchmark.js

--- loop_benchmark.js
+++ loop_benchmark.js
@@ -1,16 +1,10 @@
-const startTime = Date.now();
-const endTime = startTime + 10000; // 10 seconds later
+let count_loops = 0;
 
-let countLoops = 0;
+process.on('SIGTERM', () => {
+    console.log(`Node.js ${process.version} executed the loop ${count_loops} times before termination.`);
+    process.exit(0);
+});
 
-// Run the while loop as many times as possible within 10 seconds
-while (Date.now() < endTime) {
-    countLoops++;
+while (true) {
+    ++count_loops;
 }
-
-// Calculate the number of loop iterations performed per second
-const perSecond = countLoops / 10;
-const countLoopsFormatted = countLoops.toLocaleString('en-US');
-const perSecondFormatted = perSecond.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 });
-
-console.log(`Node.js ${process.version} executed the loop ${countLoopsFormatted} times in 10 seconds. (${perSecondFormatted}/sec)`);
