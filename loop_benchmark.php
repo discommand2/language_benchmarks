c03651e485d3d7835a62ae57a3856836824edf0c
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:19:27 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -3,13 +3,6 @@
 $cpuCores = shell_exec("nproc"); // Get the number of CPU cores
 $runtime = new \parallel\Runtime();
 $futures = [];
-pcntl_async_signals(true);
-pcntl_signal(SIGTERM, function () use (&$futures) {
-    echo ("Got here\n");
-    $total_count = 0;
-    foreach ($futures as $future) $total_count += $future->value();
-    echo "PHP " . phpversion() . " looped $total_count times.\n";
-});
 
 for ($i = 0; $i < $cpuCores; $i++) {
     $futures[] = $runtime->run(function () {
@@ -23,3 +16,11 @@ for ($i = 0; $i < $cpuCores; $i++) {
         return $count_loops;
     });
 }
+
+pcntl_async_signals(true);
+pcntl_signal(SIGTERM, function () use (&$futures) {
+    echo ("Got here\n");
+    $total_count = 0;
+    foreach ($futures as $future) $total_count += $future->value();
+    echo "PHP " . phpversion() . " looped $total_count times.\n";
+});
