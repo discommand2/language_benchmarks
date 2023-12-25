74447db01865026c9de66c39231baaf72a8ab6d8
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 16:56:41 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -5,12 +5,18 @@ use parallel\{Runtime, Future, Channel};
 $numCpus = shell_exec("nproc");
 $channel = Channel::make('loops');
 
-$ctrlcHandler = function ($signo) use ($channel) {
+$ctrlcHandler = function ($signo) use ($channel, &$runtimes) {
     $totalLoops = 0;
-    while ($channel->recv()) {
-        $totalLoops += 1;
+    while (($loops = $channel->recv()) !== null) {
+        if ($loops === 'stop') {
+            break;
+        }
+        $totalLoops += $loops;
     }
-    echo "PHP looped $totalLoops times.\n";
+    echo "PHP " . phpversion() . " looped $totalLoops times.\n";
+    foreach ($runtimes as $runtime) {
+        $runtime->kill();
+    }
     exit(0);
 };
 
@@ -25,9 +31,19 @@ for ($i = 0; $i < $numCpus; $i++) {
             for ($j = 0; $j < 1000000; $j++) {
                 // This loop will run a million times before moving on
             }
+            if ($channel->poll()) {
+                $message = $channel->recv();
+                if ($message === 'stop') {
+                    return;
+                }
+            }
             $channel->send(1000000);
         }
     }, [$channel]);
+}
+
+while (true) {
+    sleep(1);
 }
 
 foreach ($runtimes as $runtime) {
