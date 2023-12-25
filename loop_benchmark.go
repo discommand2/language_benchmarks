a39b6d61c0a8c5b59b7f55b791a79bedc56ca400
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 14:16:14 -0500

    Update loop_benchmark.go

--- loop_benchmark.go
+++ loop_benchmark.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"syscall"
 )
@@ -17,11 +19,25 @@ func main() {
 
 	go func(countLoops *int64) {
 		<-sigs
-		fmt.Printf("Go %s executed the loop %d times.\n", goVersion, *countLoops)
+		fmt.Printf("Go %s executed the loop %s times.\n", goVersion, comma(*countLoops))
 		os.Exit(0)
 	}(&countLoops)
 
 	for {
 		atomic.AddInt64(&countLoops, 1)
 	}
+}
+
+func comma(v int64) string {
+	s := strconv.FormatInt(v, 10)
+	var parts []string
+	for len(s) > 0 {
+		length := len(s)
+		if length > 3 {
+			length = 3
+		}
+		parts = append([]string{s[len(s)-length:]}, parts...)
+		s = s[:len(s)-length]
+	}
+	return strings.Join(parts, ",")
 }
