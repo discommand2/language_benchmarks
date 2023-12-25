9d590891a9ecbfeb71a7786e3391586d57944c65
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:59:50 -0500

    Update loop_benchmark.cpp

--- loop_benchmark.cpp
+++ loop_benchmark.cpp
@@ -1,10 +1,12 @@
 #include <csignal>
 #include <iostream>
 #include <atomic>
+#include <locale>
 
 std::atomic<unsigned long long> count_loops(0);
 
 void signalHandler(int signum) {
+    std::cout.imbue(std::locale("en_US.UTF-8"));
     std::cout << "C++ executed the loop " << count_loops << " times before termination." << std::endl;
     exit(signum);
 }
