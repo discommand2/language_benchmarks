ebb00c704a4e6f3c275e57ed548d790dafdde3cf
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:58:36 -0500

    Update loop_benchmark.cpp

--- loop_benchmark.cpp
+++ loop_benchmark.cpp
@@ -1,27 +1,18 @@
+#include <csignal>
 #include <iostream>
-#include <chrono>
-#include <iomanip>
-#include <locale>
+#include <atomic>
+
+std::atomic<unsigned long long> count_loops(0);
+
+void signalHandler(int signum) {
+    std::cout << "C++ executed the loop " << count_loops << " times before termination." << std::endl;
+    exit(signum);
+}
 
 int main() {
-    auto start_time = std::chrono::high_resolution_clock::now();
-    auto end_time = start_time + std::chrono::seconds(10);
-
-    long long count_loops = 0;
-
-    // Run the while loop as many times as possible within 10 seconds
-    while (std::chrono::high_resolution_clock::now() < end_time) {
-        count_loops++;
+    signal(SIGTERM, signalHandler);
+    while (true) {
+        ++count_loops;
     }
-
-    // Calculate iterations per second
-    double per_second = static_cast<double>(count_loops) / 10.0;
-
-    // Output the results with locale formatting for thousands separator
-    std::cout.imbue(std::locale(""));
-    std::cout << "C++ (version " << __VERSION__ << ") executed the loop " << count_loops 
-              << " times in 10 seconds. (" << std::fixed 
-              << std::setprecision(1) << per_second << "/sec)" << std::endl;
-
     return 0;
 }
