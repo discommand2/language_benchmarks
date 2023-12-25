5b384173c62305a4dcf2ab6c3f644131716c83bd
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:59:25 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -4,14 +4,16 @@ use parallel\{Runtime, Future, Channel};
 
 $cpuCount = shell_exec("nproc");
 $channel = Channel::make('loopCounter', Channel::Infinite);
+$shouldStop = Channel::make('shouldStop');
 
 pcntl_async_signals(true);
-$ctrlcHandler = function ($signo) use ($channel) {
+$ctrlcHandler = function ($signo) use ($channel, $shouldStop) {
     $totalLoops = 0;
     while ($channel->recv($totalLoops, false)) {
         $totalLoops += $totalLoops;
     }
     echo "PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
+    $shouldStop->send(true); // Signal threads to stop
     exit(0);
 };
 pcntl_signal(SIGTERM, $ctrlcHandler);
@@ -19,7 +21,7 @@ pcntl_signal(SIGTERM, $ctrlcHandler);
 $runtimes = [];
 for ($i = 0; $i < $cpuCount; $i++) {
     $runtimes[$i] = new Runtime();
-    $runtimes[$i]->run(function ($channel, $i) {
+    $runtimes[$i]->run(function ($channel, $i, $shouldStop) {
         $countLoops = 0;
         while (true) {
             for ($j = 0; $j < 1_000_000; $j++) {
@@ -28,8 +30,11 @@ for ($i = 0; $i < $cpuCount; $i++) {
             $countLoops += 1_000_000;
             $channel->send($countLoops);
             echo ("1,000,000 loops in thread $i\n");
+            if ($shouldStop->poll()) { // Check if we should stop
+                return;
+            }
         }
-    }, [$channel, $i]);
+    }, [$channel, $i, $shouldStop]);
 }
 
 foreach ($runtimes as $runtime) {
