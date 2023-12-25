f1a641b11e01d495edd31bc5e5bf8ab580ddd99e
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 14:15:15 -0500

    Update loop_benchmark.go

--- loop_benchmark.go
+++ loop_benchmark.go
@@ -4,23 +4,24 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync/atomic"
 	"syscall"
 )
 
 var goVersion string
 
 func main() {
-	countLoops := 0
+	var countLoops int64
 	sigs := make(chan os.Signal, 1)
 	signal.Notify(sigs, syscall.SIGTERM)
 
-	go func(countLoops *int) {
+	go func(countLoops *int64) {
 		<-sigs
 		fmt.Printf("Go %s executed the loop %d times.\n", goVersion, *countLoops)
 		os.Exit(0)
 	}(&countLoops)
 
 	for {
-		countLoops++
+		atomic.AddInt64(&countLoops, 1)
 	}
 }
