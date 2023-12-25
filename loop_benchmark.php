664b4ec51c57a32b4385c88acc045d87d3f76c64
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:24:15 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -7,18 +7,10 @@ $futures = [];
 for ($i = 0; $i < $cpuCores; $i++) {
     $futures[] = $runtime->run(function () {
         $count_loops = 0;
-        while (true) ++$count_loops;
+        while ($count_loops < 1000000) ++$count_loops;
         return $count_loops;
     });
 }
-
-pcntl_async_signals(true);
-pcntl_signal(SIGTERM, function () use (&$futures) {
-    echo ("Got here\n");
-    $total_count = 0;
-    foreach ($futures as $future) {
-        $future->cancel();
-        $total_count += $future->value();
-    }
-    echo "PHP " . phpversion() . " looped $total_count times.\n";
-});
+$total_count = 0;
+foreach ($futures as $future) $total_count += $future->value();
+echo ("PHP " . phpversion() . " looped $total_count times\n");
