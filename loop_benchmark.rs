7c1e3a9f8d9e91194e513164a60d8a3f0605b169
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 16:17:00 -0500

    Update loop_benchmark.rs

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,23 +1,38 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
+use std::thread;
 use ctrlc::set_handler;
 use rustc_version::{version};
 use num_format::{Locale, ToFormattedString};
 
 fn main() {
     let count_loops = Arc::new(AtomicUsize::new(0));
-    let count_loops_clone = Arc::clone(&count_loops);
 
-    set_handler(move || {
-        println!("Rust {} incremented {} times.",
-                 version().unwrap(),
-                 count_loops_clone.load(Ordering::SeqCst).to_formatted_string(&Locale::en));
-        std::process::exit(0);
+    set_handler({
+        let count_loops_clone = Arc::clone(&count_loops);
+        move || {
+            println!("Rust {} incremented {} times.",
+                     version().unwrap(),
+                     count_loops_clone.load(Ordering::SeqCst).to_formatted_string(&Locale::en));
+            std::process::exit(0);
+        }
     }).expect("Error setting Ctrl-C handler");
 
-    let mut local_counter = 0;
-    loop {
-        local_counter += 1;
-        count_loops.store(local_counter, Ordering::SeqCst);
+    let mut handles = vec![];
+
+    for _ in 0..4 {
+        let count_loops_clone = Arc::clone(&count_loops);
+        let handle = thread::spawn(move || {
+            let mut local_counter = 0;
+            loop {
+                local_counter += 1;
+                count_loops_clone.fetch_add(1, Ordering::SeqCst);
+            }
+        });
+        handles.push(handle);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
     }
 }
