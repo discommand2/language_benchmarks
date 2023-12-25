c11cd3e957484d38b9e7b380505904481a04302c
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 14:10:02 -0500

    x

---
+++ loop_benchmark.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	countLoops := 0
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+		fmt.Printf("Go %s executed the loop %d times.\n", goVersion(), countLoops)
+		os.Exit(0)
+	}()
+
+	for {
+		countLoops++
+		time.Sleep(time.Millisecond) // This is to prevent the loop from consuming too much CPU
+	}
+}
+
+func goVersion() string {
+	return runtime.Version()
+}
