2655a6503682be3145e98a13a2fb3658f0cffddc
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 18:40:10 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -1,6 +1,6 @@
 <?php
 
-use parallel\{Runtime, Future, Channel};
+use parallel\{Runtime, Channel};
 
 $cpuCount = shell_exec("nproc");
 $channel = Channel::make('loopCounter', Channel::Infinite);
@@ -10,22 +10,21 @@ $futures = [];
 pcntl_async_signals(true);
 pcntl_signal(SIGTERM, function ($signo) use ($totalLoops, $runtimes, $futures) {
     echo ("Got here!");
+    echo "1 PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
     foreach ($futures as $future) $future->kill();
     foreach ($runtimes as $runtime) $runtime->close();
-    echo "PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
+    echo "2 PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
     exit(0);
 });
 
 for ($i = 0; $i < $cpuCount; $i++) {
     $runtimes[$i] = new Runtime();
     $futures[$i] = $runtimes[$i]->run(function ($channel, $i) {
-        $countLoops = 0;
         while (true) {
             for ($j = 0; $j < 1_000_000; $j++) {
                 // This loop will run a million times before moving on
             }
-            $countLoops += 1_000_000;
-            $channel->send($countLoops);
+            $channel->send(1_000_000);
             echo ("1,000,000 loops in thread $i\n");
         }
     }, [$channel, $i]);
