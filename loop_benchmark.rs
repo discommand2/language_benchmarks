8f7fa30603e5503ca906b2a50271924e85cd70c5
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 12:33:45 -0500

    Update loop_benchmark.rs

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,4 +1,4 @@
-use std::sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex, Condvar};
+use std::sync::{atomic::{AtomicUsize, Ordering}, Arc, Mutex};
 use std::io::{self, Write, Error};
 use signal_hook::{iterator::Signals, consts::SIGTERM};
 use rustc_version::{version};
@@ -7,8 +7,8 @@ fn main() -> Result<(), Error> {
     let count_loops = Arc::new(AtomicUsize::new(0));
     let count_loops_clone = Arc::clone(&count_loops);
 
-    let pair = Arc::new((Mutex::new(false), Condvar::new()));
-    let pair2 = Arc::clone(&pair);
+    let started = Arc::new(Mutex::new(false));
+    let started_clone = Arc::clone(&started);
 
     let mut signals = Signals::new(&[SIGTERM])?;
 
@@ -19,18 +19,15 @@ fn main() -> Result<(), Error> {
                      count_loops_clone.load(Ordering::Relaxed));
             io::stdout().flush().unwrap();
 
-            let (lock, _cvar) = &*pair2;
-            let started = lock.lock().unwrap();
+            let mut started = started_clone.lock().unwrap();
             *started = true;
-            cvar.notify_one();
         }
     });
 
     loop {
         count_loops.fetch_add(1, Ordering::Relaxed);
 
-        let (lock, _cvar) = &*pair;
-        let started = lock.lock().unwrap();
+        let started = started.lock().unwrap();
         if *started {
             break;
         }
