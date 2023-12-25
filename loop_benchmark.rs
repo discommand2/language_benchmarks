4102dc9ff05268699e5a3323a19a4da232b5f3ff
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 13:31:47 -0500

    x

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,25 +1,18 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use signal_hook::{consts::SIGTERM, iterator::Signals};
+use ctrlc::set_handler;
 
 fn main() {
-    // Create a shared atomic counter
     let count_loops = Arc::new(AtomicUsize::new(0));
-
-    // Clone the counter to be used in the signal handler
     let count_loops_clone = Arc::clone(&count_loops);
 
-    // Set up the signal handler for SIGTERM
-    let mut signals = Signals::new(&[SIGTERM]).expect("Error setting up signal handler");
-    std::thread::spawn(move || {
-        for _ in signals.forever() {
-            println!("Rust executed the loop {} times before termination.",
-                     count_loops_clone.load(Ordering::Relaxed));
-            std::process::exit(0);
-        }
-    });
+    set_handler(move || {
+        println!("Rust {} executed the loop {} times before termination.",
+                 std::env::consts::RUST_VERSION,
+                 count_loops_clone.load(Ordering::Relaxed));
+        std::process::exit(0);
+    }).expect("Error setting Ctrl-C handler");
 
-    // Loop indefinitely, incrementing the counter
     loop {
         count_loops.fetch_add(1, Ordering::Relaxed);
     }
