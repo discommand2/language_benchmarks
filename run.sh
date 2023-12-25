3cfdf175d39297cd938d6843b418a5cae5682d47
Russell Purinton <russell.purinton@icloud.com>
2023-12-25 17:00:43 +0000

    push

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 timeout 10s php loop_benchmark.php
-python loop_benchmark.py
-node loop_benchmark.js
-./loop_benchmark_c++
+timeout 10s python loop_benchmark.py
+timeout 10s node loop_benchmark.js
+timeout 10s ./loop_benchmark_c++
 java LoopBenchmark
 ./loop_benchmark_rust
 ./loop_benchmark_go
