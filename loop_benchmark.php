502023c4509b60204e6378523df460f6b1600403
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:54:35 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -1,63 +1,37 @@
 <?php
 
-use parallel\{Channel, Runtime, Events};
+use parallel\{Runtime, Future, Channel};
 
-// Function that each thread will execute
-$threadFunction = function (Channel $channel) {
-    $countLoops = 0;
-    while (true) {
-        for ($i = 0; $i < 1_000_000; $i++) {
-            // This loop will run a million times before moving on
-        }
-        $countLoops += 1_000_000;
-        $channel->send($countLoops);
-    }
-};
+$cpuCount = shell_exec("nproc");
+$channel = Channel::make('loopCounter', Channel::Infinite);
 
-// Create an event loop to listen for messages from threads
-$events = new Events();
-
-// Create a channel for communication
-$channel = Channel::make('countLoops', Channel::Infinite);
-
-// Create an array to store the runtime objects
-$runtimes = [];
-
-// Create threads equal to the number of CPUs
-$cpuCount = shell_exec('nproc') ?: 1;
-for ($i = 0; $i < $cpuCount; $i++) {
-    $runtime = new Runtime();
-    $runtimes[] = $runtime;
-    $runtime->run($threadFunction, [$channel]);
-    $events->addChannel($channel);
-}
-
-// Register a signal handler for SIGINT (Ctrl+C)
 pcntl_async_signals(true);
-pcntl_signal(SIGTERM, function () use ($channel) {
+$ctrlcHandler = function ($signo) use ($channel) {
     $totalLoops = 0;
-    while ($channel->count()) {
-        $totalLoops += $channel->recv();
+    while ($channel->recv($totalLoops, false)) {
+        $totalLoops += $totalLoops;
     }
-    echo "PHP looped " . number_format($totalLoops) . " times.\n";
+    echo "PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
     exit(0);
-});
+};
+pcntl_signal(SIGTERM, $ctrlcHandler);
 
-// Event loop to accumulate the count of loops from all threads
-$totalLoops = 0;
-while (true) {
-    $event = $events->poll();
-
-    if ($event->type === Events\Event\Type::Read) {
-        $totalLoops += $event->value;
-        $events->addChannel($channel);
-    }
-
-    // You can add some logic here to break the loop if needed
+$runtimes = [];
+for ($i = 0; $i < $cpuCount; $i++) {
+    $runtimes[$i] = new Runtime();
+    $runtimes[$i]->run(function ($channel, $i) {
+        $countLoops = 0;
+        while (true) {
+            echo ($i);
+            for ($j = 0; $j < 1000000; $j++) {
+                // This loop will run a million times before moving on
+            }
+            $countLoops += 1000000;
+            $channel->send($countLoops);
+        }
+    }, [$channel, $i]);
 }
 
-// Cleanup
 foreach ($runtimes as $runtime) {
     $runtime->close();
 }
-Channel::close($channel);
