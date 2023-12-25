fef20561eb1e825d92610c878e1cbd67618c59d3
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:54:49 -0500

    Update loop_benchmark.py

--- loop_benchmark.py
+++ loop_benchmark.py
@@ -1,13 +1,20 @@
 import signal
 import sys
 
-count_loops = 0
+def main():
+    count_loops = 0
 
-def handler(signum, frame):
-    print(f"Python {sys.version_info.major}.{sys.version_info.minor} executed the loop {count_loops:,} times before termination.")
-    sys.exit(0)
+    def handler(signum, frame):
+        print(f"Python {sys.version_info.major}.{sys.version_info.minor} executed the loop {count_loops:,} times before termination.")
+        sys.exit(0)
 
-signal.signal(signal.SIGTERM, handler)
+    signal.signal(signal.SIGTERM, handler)
 
-while True:
-    count_loops += 1
+    try:
+        while True:
+            count_loops += 1
+    finally:
+        print(f"Python {sys.version_info.major}.{sys.version_info.minor} executed the loop {count_loops:,} times before termination.")
+
+if __name__ == "__main__":
+    main()
