8833c7d6b656e24e6e485c2761fb2aaff93dd122
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 12:10:56 -0500

    Update LoopBenchmark.java

--- LoopBenchmark.java
+++ LoopBenchmark.java
@@ -1,15 +1,17 @@
+import java.util.concurrent.atomic.LongAdder;
+
 public class LoopBenchmark {
-    private static volatile long countLoops = 0;
+    private static final LongAdder countLoops = new LongAdder();
 
     public static void main(String[] args) {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
-                System.out.format("Java %s executed the loop %,d times before termination.%n", System.getProperty("java.version"), countLoops);
+                System.out.format("Java %s executed the loop %,d times before termination.%n", System.getProperty("java.version"), countLoops.sum());
             }
         });
 
         while (true) {
-            countLoops++;
+            countLoops.increment();
         }
     }
 }
