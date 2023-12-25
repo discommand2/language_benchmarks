1ea4dbf0a87fcadbb947db99ac70c38e381e8070
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 17:38:02 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -10,6 +10,7 @@ for ($i = 0; $i < 8; $i++) {
         return 1;
     }, [$i]);
 }
+for ($j = 0; $j < 500; $j++) echo ($i);
 $total_count = 0;
 foreach ($futures as $future) $total_count += $future->value();
 echo ("PHP " . phpversion() . " looped " . number_format($total_count) . " times.\n");
