9a7978f199f3ef3bcd5108ee361157f945ae62ee
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:36:55 -0500

    Update loop_benchmark.js

--- loop_benchmark.js
+++ loop_benchmark.js
@@ -1,10 +1,19 @@
 let count_loops = 0;
+let running = true;
 
 process.on('SIGTERM', () => {
+    running = false;
     console.log(`Node.js ${process.version} executed the loop ${count_loops} times before termination.`);
     process.exit(0);
 });
 
-while (true) {
-    ++count_loops;
+function increment() {
+    let i = 0;
+    while (i < 1e6 && running) {
+        ++count_loops;
+        ++i;
+    }
+    if (running) setImmediate(increment);
 }
+
+increment();
