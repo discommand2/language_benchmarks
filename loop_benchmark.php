b8c7f6e58fefcb8720b35c142404e3bbaca78be8
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:26:14 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -1,7 +1,8 @@
 <?php
+// called by: 'timeout 10s php loop_benchmark.php'
 $count_loops = 0;
 pcntl_async_signals(true);
-pcntl_signal(SIGTERM, function() use (&$count_loops) {
+pcntl_signal(SIGTERM, function () use (&$count_loops) {
     die("PHP " . phpversion() . " executed the loop " . number_format($count_loops) . " times in 10 seconds.\n");
 });
-while (true) $count_loops++;
+while (true) ++$count_loops;
