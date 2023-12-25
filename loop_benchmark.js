231bcd92d614053ecee7561866adb79d2593499e
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 11:31:48 -0500

    Revert "Update loop_benchmark.js"
    This reverts commit e2f6274651d2f9622ddb25ec1db764ec0f62af50.

--- loop_benchmark.js
+++ loop_benchmark.js
@@ -1,10 +1,16 @@
-let count_loops = 0;
+const startTime = Date.now();
+const endTime = startTime + 10000; // 10 seconds later
 
-process.on('SIGTERM', () => {
-    console.log(`Node.js ${process.version} executed the loop ${count_loops} times before termination.`);
-    process.exit(0);
-});
+let countLoops = 0;
 
-setInterval(() => {
-    ++count_loops;
-}, 0);
+// Run the while loop as many times as possible within 10 seconds
+while (Date.now() < endTime) {
+    countLoops++;
+}
+
+// Calculate the number of loop iterations performed per second
+const perSecond = countLoops / 10;
+const countLoopsFormatted = countLoops.toLocaleString('en-US');
+const perSecondFormatted = perSecond.toLocaleString('en-US', { minimumFractionDigits: 1, maximumFractionDigits: 1 });
+
+console.log(`Node.js ${process.version} executed the loop ${countLoopsFormatted} times in 10 seconds. (${perSecondFormatted}/sec)`);
