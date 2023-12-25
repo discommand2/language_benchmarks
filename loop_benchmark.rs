cc4873eac630c7d4fae06bab725afdd7d96eb360
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 12:27:24 -0500

    Update loop_benchmark.rs

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,18 +1,23 @@
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
-use ctrlc::set_handler;
+use std::io::Error;
+use signal_hook::{iterator::Signals, SIGTERM};
 use rustc_version::{version};
 
-fn main() {
+fn main() -> Result<(), Error> {
     let count_loops = Arc::new(AtomicUsize::new(0));
     let count_loops_clone = Arc::clone(&count_loops);
 
-    set_handler(move || {
-        println!("Rust {} executed the loop {} times before termination.",
-                 version().unwrap(),
-                 count_loops_clone.load(Ordering::Relaxed));
-        std::process::exit(0);
-    }).expect("Error setting Ctrl-C handler");
+    let signals = Signals::new(&[SIGTERM])?;
+
+    std::thread::spawn(move || {
+        for _ in signals.forever() {
+            println!("Rust {} executed the loop {} times before termination.",
+                     version().unwrap(),
+                     count_loops_clone.load(Ordering::Relaxed));
+            std::process::exit(0);
+        }
+    });
 
     loop {
         count_loops.fetch_add(1, Ordering::Relaxed);
