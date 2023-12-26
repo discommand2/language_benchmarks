b8fb75689ac224ae62d0846b7290dda9a25588d8
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 22:04:11 -0500

    Update loop_benchmark.cpp

--- loop_benchmark.cpp
+++ loop_benchmark.cpp
@@ -1,19 +1,60 @@
-#include <csignal>
 #include <iostream>
-#include <locale>
+#include <vector>
+#include <thread>
+#include <atomic>
+#include <csignal>
+#include <unistd.h>
+#include <iomanip>
+#include <sstream>
 
-volatile unsigned long long count_loops = 0;
+std::atomic<bool> running(true);
+std::atomic<unsigned long long> totalLoops(0);
 
 void signalHandler(int signum) {
-    std::cout.imbue(std::locale("en_US.UTF-8"));
-    std::cout << "C++ " << __GNUC__ << "." << __GNUC_MINOR__ << "." << __GNUC_PATCHLEVEL__ << " incremented " << count_loops << " times." << std::endl;
-    exit(signum);
+    running = false;
+}
+
+void loopFunction() {
+    unsigned long long localLoops = 0;
+    while (running) {
+        for (int j = 0; j < 5000000; ++j) {
+            // Just a busy loop.
+        }
+        localLoops += 5000000;
+    }
+    totalLoops += localLoops;
+}
+
+std::string formatNumber(unsigned long long number) {
+    std::stringstream ss;
+    ss.imbue(std::locale(""));
+    ss << std::fixed << number;
+    return ss.str();
 }
 
 int main() {
-    signal(SIGTERM, signalHandler);
-    while (true) {
-        ++count_loops;
+    // Register signal handler for SIGINT and SIGTERM
+    std::signal(SIGINT, signalHandler);
+    std::signal(SIGTERM, signalHandler);
+
+    // Get the number of CPU cores
+    unsigned int cpuCount = std::thread::hardware_concurrency();
+
+    // Create a vector to hold the threads
+    std::vector<std::thread> threads;
+
+    // Launch a thread for each CPU core
+    for (unsigned int i = 0; i < cpuCount; ++i) {
+        threads.emplace_back(loopFunction);
     }
+
+    // Wait for all threads to finish
+    for (auto& thread : threads) {
+        thread.join();
+    }
+
+    // Output the result
+    std::cout << "C++ looped " << formatNumber(totalLoops) << " times." << std::endl;
+
     return 0;
 }
