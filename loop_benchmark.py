188b1c617da8b69cfa4135e12bebf2bb01af7cd1
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 23:21:07 -0500

    Update loop_benchmark.py

--- loop_benchmark.py
+++ loop_benchmark.py
@@ -1,7 +1,21 @@
 import os
 import signal
-from multiprocessing import cpu_count, Process, Value
+from multiprocessing import Process, Value, cpu_count, current_process
+from ctypes import c_uint
 from platform import python_version
+
+# Atomic counter that can be safely incremented from multiple processes
+class LoopBenchmark:
+    def __init__(self):
+        self.value = Value(c_uint, 0)  # Shared among processes
+
+    def increment(self, amount):
+        with self.value.get_lock():
+            self.value.value += amount
+
+    def get(self):
+        with self.value.get_lock():
+            return self.value.value
 
 # Function to be run by each process
 def loop_function(counter):
@@ -11,32 +25,43 @@ def loop_function(counter):
         for _ in range(5_000_000):
             pass
         local_counter += 5_000_000
-        with counter.get_lock():
-            counter.value += 5_000_000
+        counter.increment(5_000_000)
 
 # Signal handler for graceful shutdown
 def shutdown_handler(signum, frame, counter):
-    print(f"Python {python_version()} looped {counter.value:,} times.")
+    print(f"Python {python_version()} looped {counter.get():,} times.")
     os._exit(0)
 
 # Main function
 def main():
-    counter = Value('i', 0)
+    counter = LoopBenchmark()
     cpu_count_value = cpu_count()
 
     # Register signal handlers
-    signal.signal(signal.SIGINT, lambda signum, frame: shutdown_handler(signum, frame, counter))
-    signal.signal(signal.SIGTERM, lambda signum, frame: shutdown_handler(signum, frame, counter))
+    original_sigint_handler = signal.signal(signal.SIGINT, signal.SIG_IGN)
+    original_sigterm_handler = signal.signal(signal.SIGTERM, signal.SIG_IGN)
 
     # Create a process pool with one process per CPU core
     processes = []
     for _ in range(cpu_count_value):
-        p = Process(target=loop_function, args=(counter,))
-        p.start()
-        processes.append(p)
+        process = Process(target=loop_function, args=(counter,))
+        process.start()
+        processes.append(process)
 
-    for p in processes:
-        p.join()
+    # Restore the original signal handlers
+    signal.signal(signal.SIGINT, original_sigint_handler)
+    signal.signal(signal.SIGTERM, original_sigterm_handler)
+
+    # Register signal handlers for the main process
+    signal.signal(signal.SIGINT, lambda signum, frame: shutdown_handler(signum, frame, counter))
+    signal.signal(signal.SIGTERM, lambda signum, frame: shutdown_handler(signum, frame, counter))
+
+    # Wait for all processes to complete
+    try:
+        for process in processes:
+            process.join()
+    except KeyboardInterrupt:
+        shutdown_handler(signal.SIGINT, None, counter)
 
 if __name__ == "__main__":
     main()
