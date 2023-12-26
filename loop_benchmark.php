184d653a5fab8901fbf378d43f4f9ee68e69abec
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 19:06:38 -0500

    Update loop_benchmark.php

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -24,10 +24,6 @@ for ($i = 0; $i < $cpuCount; $i++) {
     $runtimes[$i] = new Runtime();
     $futures[$i] = $runtimes[$i]->run(function ($channel, $i) {
         pcntl_async_signals(true);
-        $handler = function ($signo) use ($i) {
-            echo "SIGNAL $signo PHP (Thread $i)";
-        };
-        pcntl_signal(SIGINT, $handler);
         while (true) {
             for ($j = 0; $j < 1_000_000; $j++) {
                 // This loop will run a million times before moving on
