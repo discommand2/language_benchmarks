5b7edfff4717d527cb1651b36c5dcf58ca8e9f54
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 22:14:35 -0500

    Update LoopBenchmark.java

--- LoopBenchmark.java
+++ LoopBenchmark.java
@@ -1,17 +1,48 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicLong;
+import java.util.concurrent.TimeUnit;
 
 public class LoopBenchmark {
-    private static volatile long countLoops = 0;
+    private static final AtomicLong totalLoops = new AtomicLong(0);
 
     public static void main(String[] args) {
-        Runtime.getRuntime().addShutdownHook(new Thread() {
-            public void run() {
-                System.out.format("Java %s incremented %,d times.%n", System.getProperty("java.version"), countLoops);
-            }
-        });
+        int cpuCount = Runtime.getRuntime().availableProcessors();
+        ExecutorService executor = Executors.newFixedThreadPool(cpuCount);
 
-        while (true) {
-            countLoops++;
+        // Register shutdown hook
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Java " + System.getProperty("java.version") + " looped " + String.format("%,d", totalLoops.get()) + " times.");
+            executor.shutdownNow(); // Attempt to stop all actively executing tasks
+            try {
+                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                    System.err.println("Executor did not terminate in the allotted time.");
+                }
+            } catch (InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }));
+
+        // Start threads
+        for (int i = 0; i < cpuCount; i++) {
+            executor.submit(() -> {
+                while (!Thread.currentThread().isInterrupted()) {
+                    for (int j = 0; j < 5_000_000; j++) {
+                        // Loop body can be empty or do some work
+                    }
+                    totalLoops.addAndGet(5_000_000);
+                }
+            });
+        }
+
+        // Await termination of the executor (which should never happen in normal operation)
+        executor.shutdown();
+        try {
+            while (!executor.awaitTermination(24L, TimeUnit.HOURS)) {
+                System.out.println("Still waiting for termination...");
+            }
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
         }
     }
 }
