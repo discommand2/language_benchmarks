4a94088b3fa45fab10a7bc75b18bcd0cad4e2a8c
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 21:07:10 -0500

    Update loop_benchmark.rs

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -34,8 +34,8 @@ fn main() {
             sched_setaffinity(Pid::from_raw(0), &cpuset).expect("Failed to set affinity");
             let mut j = 0;
             loop {
-                for (j, _) in (0..5_000_000).enumerate() {
-                    // That's it. This is the loop.
+                for _ in 0..5_000_000 {
+                    j += 1;
                 }
                 count_loops_clone.fetch_add(5_000_000, Ordering::Relaxed);
             }
