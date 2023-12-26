c49fc22e62a8f8a1c524c1d27137c2b68a332dbe
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 22:35:55 -0500

    Update loop_benchmark.js

--- loop_benchmark.js
+++ loop_benchmark.js
@@ -1,19 +1,44 @@
-let i = 0;
-let count_loops = 0;
-process.on('SIGTERM', () => {
-    count_loops = (count_loops * 1e6) + count_loops;
-    let count_loops_formatted = count_loops.toLocaleString();
-    console.log(`Node.js ${process.version} incremented ${count_loops_formatted} times.`);
+const { Worker, isMainThread, parentPort, workerData } = require('worker_threads');
+const os = require('os');
+
+if (isMainThread) {
+  const cpuCount = os.cpus().length;
+  let totalLoops = 0;
+  const workers = [];
+
+  process.on('SIGINT', shutdown);
+  process.on('SIGTERM', shutdown);
+
+  for (let i = 0; i < cpuCount; i++) {
+    const worker = new Worker(__filename);
+    workers.push(worker);
+
+    worker.on('message', (loops) => {
+      totalLoops += loops;
+    });
+
+    worker.on('error', (err) => {
+      console.error(err);
+      shutdown();
+    });
+
+    worker.on('exit', (code) => {
+      if (code !== 0) {
+        console.error(`Worker stopped with exit code ${code}`);
+        shutdown();
+      }
+    });
+  }
+
+  function shutdown() {
+    console.log(`Node.js looped ${totalLoops.toLocaleString()} times.`);
+    workers.forEach(worker => worker.terminate());
     process.exit(0);
-});
-
-function increment() {
-    while (i < 1e6) {
-        ++i;
-    }
-    i = 0;
-    ++count_loops;
-    setImmediate(increment);
+  }
+} else {
+  let loops = 0;
+  while (true) {
+    for(let i = 0; i < 5000000; i++);
+    parentPort.postMessage(5000000);
+  }
 }
-
-increment();
