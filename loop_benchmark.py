f9c94bcb481f5c6d675dfff401d563294dbd17d9
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-25 22:28:45 -0500

    Update loop_benchmark.py

--- loop_benchmark.py
+++ loop_benchmark.py
@@ -1,16 +1,54 @@
+import os
 import signal
-import sys
+import threading
+from concurrent.futures import ThreadPoolExecutor
+from multiprocessing import cpu_count
+from platform import python_version
+from ctypes import c_uint
 
-def main():
-    count_loops = 0
+# Atomic counter that can be safely incremented from multiple threads
+class LoopBenchmark:
+    def __init__(self):
+        self.value = c_uint(0)
+        self._lock = threading.Lock()
 
-    def handler(signum, frame):
-        print(f"Python {sys.version_info.major}.{sys.version_info.minor} incremented {count_loops:,} times.")
-        sys.exit(0)
+    def increment(self, amount):
+        with self._lock:
+            self.value.value += amount
+            return self.value.value
 
-    signal.signal(signal.SIGTERM, handler)
+    def get(self):
+        with self._lock:
+            return self.value.value
 
+# Function to be run by each thread
+def loop_function(counter):
+    local_counter = 0
     while True:
-        count_loops += 1
+        # Simulate some work
+        for _ in range(5_000_000):
+            pass
+        local_counter += 5_000_000
+        counter.increment(5_000_000)
 
-main()
+# Signal handler for graceful shutdown
+def shutdown_handler(signum, frame, counter):
+    print(f"Python {python_version()} looped {counter.get():,} times.")
+    os._exit(0)
+
+# Main function
+def main():
+    counter = LoopBenchmark()
+    cpu_count_value = cpu_count()
+
+    # Register signal handlers
+    signal.signal(signal.SIGINT, lambda signum, frame: shutdown_handler(signum, frame, counter))
+    signal.signal(signal.SIGTERM, lambda signum, frame: shutdown_handler(signum, frame, counter))
+
+    # Create a thread pool with one thread per CPU core
+    with ThreadPoolExecutor(max_workers=cpu_count_value) as executor:
+        for _ in range(cpu_count_value):
+            executor.submit(loop_function, counter)
+
+if __name__ == "__main__":
+    main()
