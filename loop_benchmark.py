0eded19ad52609a29744c8b63ebdea6d0eee68a9
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-27 17:34:55 -0500

    Update loop_benchmark.py

--- loop_benchmark.py
+++ loop_benchmark.py
@@ -17,9 +17,11 @@ def worker_main(worker_id, total_loops_pipe):
 
 
 def shutdown(signum, frame, workers, total_loops_pipe):
-    print(f"Python {sys.version.split()[0]} looped {format(total_loops, ',')} times.")
     for worker in workers:
         if os.getpid() == worker._parent_pid:  # Check if current process is parent
+            print(
+                f"Python {sys.version.split()[0]} looped {format(total_loops, ',')} times."
+            )
             if worker.is_alive():
                 if worker._popen is not None:
                     worker.terminate()
