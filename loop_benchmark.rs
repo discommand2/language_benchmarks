5c18ffc06b4f6f325a8ec7f4392264424fefdb55
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-27 17:01:48 -0500

    Update loop_benchmark.rs

--- loop_benchmark.rs
+++ loop_benchmark.rs
@@ -1,13 +1,12 @@
+use ctrlc::set_handler;
+use nix::sched::{sched_setaffinity, CpuSet};
+use nix::unistd::Pid;
+use num_cpus;
+use num_format::{Locale, ToFormattedString};
+use rustc_version::version;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 use std::thread;
-use num_cpus;
-use ctrlc::set_handler;
-use rustc_version::{version};
-use num_format::{Locale, ToFormattedString};
-use nix::sched::{CpuSet, sched_setaffinity};
-use nix::unistd::Pid;
-
 
 fn main() {
     let count_loops = Arc::new(AtomicUsize::new(0));
@@ -15,29 +14,34 @@ fn main() {
     set_handler({
         let count_loops_clone = Arc::clone(&count_loops);
         move || {
-            println!("Rust {} looped {} times.",
-                     version().unwrap(),
-                     count_loops_clone.load(Ordering::SeqCst).to_formatted_string(&Locale::en));
+            println!(
+                "Rust {} looped {} times.",
+                version().unwrap(),
+                count_loops_clone
+                    .load(Ordering::SeqCst)
+                    .to_formatted_string(&Locale::en)
+            );
             std::process::exit(0);
         }
-    }).expect("Error setting Ctrl-C handler");
+    })
+    .expect("Error setting Ctrl-C handler");
 
     let mut handles = vec![];
 
-    for i in 0..num_cpus::get() {
+    for i in 0..num_cpus::get() / 2 {
         let mut cpuset = CpuSet::new();
         cpuset.set(i).expect("Failed to set CPU");
 
         let count_loops_clone = Arc::clone(&count_loops);
-let handle = thread::spawn(move || {
-    sched_setaffinity(Pid::from_raw(0), &cpuset).expect("Failed to set affinity");
-    loop {
-        for _ in 0..5_000_000 {
-           // TODO: CPU busy work here
-        }
-        count_loops_clone.fetch_add(5_000_000, Ordering::Relaxed);
-    }
-});
+        let handle = thread::spawn(move || {
+            sched_setaffinity(Pid::from_raw(0), &cpuset).expect("Failed to set affinity");
+            loop {
+                for _ in 0..5_000_000 {
+                    // TODO: CPU busy work here
+                }
+                count_loops_clone.fetch_add(5_000_000, Ordering::Relaxed);
+            }
+        });
         handles.push(handle);
     }
 
