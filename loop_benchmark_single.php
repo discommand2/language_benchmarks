ae877ab3fb7aa98c1f509675feec58c36d43b3eb
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-27 16:35:23 -0500

    Update loop_benchmark_single.php

--- loop_benchmark_single.php
+++ loop_benchmark_single.php
@@ -1,19 +1,9 @@
 <?php
-
 $totalLoops = 0;
-
 $shutdownFunction = function () use (&$totalLoops) {
-    echo "PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
-    exit(0);
+    die("PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n");
 };
-
 pcntl_async_signals(true);
 pcntl_signal(SIGINT, $shutdownFunction);
 pcntl_signal(SIGTERM, $shutdownFunction);
-
-while (true) {
-    for ($j = 0; $j < 5_000_000; ++$j) {
-        // TODO: CPU busy work here
-    }
-    $totalLoops += 5_000_000;
-}
+while (true) ++$totalLoops;
