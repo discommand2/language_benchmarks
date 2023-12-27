a2e7a47b13ebda2ca58f036384be114fc026619f
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-27 16:43:49 -0500

    Update loop_benchmark_single.php

--- loop_benchmark_single.php
+++ loop_benchmark_single.php
@@ -6,4 +6,6 @@ $shutdownFunction = function () use (&$totalLoops) {
 pcntl_async_signals(true);
 pcntl_signal(SIGINT, $shutdownFunction);
 pcntl_signal(SIGTERM, $shutdownFunction);
-for($totalLoops = 0; true; ++$totalLoops);
+for ($totalLoops = 0; true; $totalLoops += 5_000_000) {
+    for ($j = 0; $j < 5_000_000; ++$j);
+}
