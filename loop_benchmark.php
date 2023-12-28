ee01b03ed2c041249732ad8e5d500b8d673e5581
Laozi 老子 <97992269+rpurinton@users.noreply.github.com>
2023-12-28 15:09:48 -0500

    Revert "Update loop_benchmark.php"
    This reverts commit bfa2978010dd4aa079ae76a138b35b3d1e8b0488.

--- loop_benchmark.php
+++ loop_benchmark.php
@@ -8,7 +8,8 @@ $futures = [];
 $cpuCount = shell_exec("nproc");
 $channel = Channel::make('loopCounter', Channel::Infinite);
 
-$sigint = function () use (&$totalLoops, &$runtimes, &$futures, $channel) {
+pcntl_async_signals(true);
+pcntl_signal(SIGINT, function () use (&$totalLoops, &$runtimes, &$futures, $channel) {
     echo "PHP " . PHP_VERSION . " looped " . number_format($totalLoops) . " times.\n";
     foreach ($futures as $i => $future) {
         $future->cancel();
@@ -16,32 +17,21 @@ $sigint = function () use (&$totalLoops, &$runtimes, &$futures, $channel) {
     }
     $channel->close();
     exit(0);
-};
-
-pcntl_async_signals(true);
-pcntl_signal(SIGINT, $sigint);
-
-$run = function ($channel) {
-    pcntl_async_signals(true);
-    pcntl_signal(SIGINT, function () {
-    });
-
-    $sendRuntime = new Runtime();
-    $send = function ($value) use ($channel) {
-        $channel->send($value);
-    };
-
-    while (true) {
-        for ($j = 0; $j < 5_000_000; ++$j) {
-            // TODO: CPU busy work here
-        }
-        $sendRuntime->run($send, [$j]);
-    }
-};
+});
 
 for ($i = 0; $i < $cpuCount / 2; ++$i) {
     $runtimes[$i] = new Runtime();
-    $futures[$i] = $runtimes[$i]->run($run, [$channel]);
+    $futures[$i] = $runtimes[$i]->run(function ($channel, $i) {
+        pcntl_async_signals(true);
+        pcntl_signal(SIGINT, function () {
+        });
+        while (true) {
+            for ($j = 0; $j < 5_000_000; ++$j) {
+                // TODO: CPU busy work here
+            }
+            $channel->send(5_000_000);
+        }
+    }, [$channel, $i]);
 }
 
 while ($totalLoops += $channel->recv());
