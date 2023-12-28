ed20d0e2a9a39fcfc9cb46730c8e37af270e173e
Russell Purinton <russell.purinton@icloud.com>
2023-12-28 21:45:41 +0000

    push

--- run.sh
+++ run.sh
@@ -1,16 +1,17 @@
 echo "Testing Python 3.8..."
 timeout -s SIGINT -k 2s 10s  python3.8 loop_benchmark.py
+
 echo "Testing Python 3.9..."
 timeout -s SIGINT -k 2s 10s  python3.9 loop_benchmark.py
+
 echo "Testing Python 3.10..."
 timeout -s SIGINT -k 2s 10s  python3.10 loop_benchmark.py
+
 echo "Testing Python 3.11..."
 timeout -s SIGINT -k 2s 10s  python3.11 loop_benchmark.py
+
 echo "Testing Python 3.12..."
 timeout -s SIGINT -k 2s 10s  python3.12 loop_benchmark.py
-
-echo "Testing C++..."
-timeout -s SIGINT -k 2s 10s ./loop_benchmark_c++
 
 echo "Testing PHP..."
 timeout -s SIGINT -k 2s 10s php loop_benchmark.php
@@ -30,9 +31,9 @@ timeout -s SIGINT -k 2s 10s LoopBenchmark/bin/Release/net7.0/linux-x64/publish/L
 echo "Testing Java..."
 timeout -s SIGINT -k 2s 10s java LoopBenchmark
 
-echo "Testing Rust (Debug Build)..."
-timeout -s SIGINT -k 2s 10s target/debug/loop_benchmark_rust
-
-echo "Testing Rust (Release Build)..."
+echo "Testing Rust..."
 timeout -s SIGINT -k 2s 10s target/release/loop_benchmark_rust
+
+echo "Testing C++..."
+timeout -s SIGINT -k 2s 10s ./loop_benchmark_c++
 
