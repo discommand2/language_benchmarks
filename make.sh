65c554c639ba0e5a4ee7e8c2df360a7203ea6a37
Russell Purinton <russell.purinton@icloud.com>
2023-12-28 22:09:52 +0000

    push

--- make.sh
+++ make.sh
@@ -1,21 +1,21 @@
-echo "Compiling C++..."
-g++ -std=c++11 -O3 -o loop_benchmark_c++ loop_benchmark.cpp -pthread
+echo "Compiling C++ with maximum optimizations."
+g++ -std=c++11 -O3 -march=native -flto -o loop_benchmark_c++ loop_benchmark.cpp -pthread
 
-echo "Compiling Go..."
-go build -ldflags "-X 'main.goVersion=$(go version | cut -d " " -f 3)'" -o loop_benchmark_go loop_benchmark.go
+echo "Compiling Go with maximum optimizations."
+go build -ldflags "-s -w -X 'main.goVersion=$(go version | cut -d " " -f 3)'" -o loop_benchmark_go loop_benchmark.go
 
-echo "Compiling Java..."
+echo "Compiling Java."
 javac LoopBenchmark.java
 
-echo "Compiling Rust (Debug)..."
-cargo build
+echo "Compiling Rust with maximum optimizations and no debug symbols."
+cargo rustc --release -- -C debuginfo=0 -C target-cpu=native
 
-echo "Compiling Rust (Release)..."
-cargo build --release
-
-echo "Compiling C#..."
+echo "Compiling C#.NET with release flag and no debug symbols."
 mkdir -p LoopBenchmark
 cd LoopBenchmark
 dotnet new console --force
 cp ../loop_benchmark.cs Program.cs
-dotnet publish -c Release -r linux-x64 --self-contained true
+dotnet publish -c Release -r linux-x64 --self-contained true -p:DebugType=None -p:DebugSymbols=false
+
+echo "Compiling done."
+
--- run.sh
+++ run.sh
@@ -1,39 +1,9 @@
-echo "Testing Python 3.8..."
-timeout -s SIGINT -k 2s 10s  python3.8 loop_benchmark.py
-
-echo "Testing Python 3.9..."
-timeout -s SIGINT -k 2s 10s  python3.9 loop_benchmark.py
-
-echo "Testing Python 3.10..."
-timeout -s SIGINT -k 2s 10s  python3.10 loop_benchmark.py
-
-echo "Testing Python 3.11..."
-timeout -s SIGINT -k 2s 10s  python3.11 loop_benchmark.py
-
-echo "Testing Python 3.12..."
-timeout -s SIGINT -k 2s 10s  python3.12 loop_benchmark.py
-
-echo "Testing PHP..."
+timeout -s SIGINT -k 2s 10s  pypy3.10 loop_benchmark.py
 timeout -s SIGINT -k 2s 10s php loop_benchmark.php
-
-echo "Testing Go..."
 timeout -s SIGINT -k 2s 10s ./loop_benchmark_go
-
-echo "Testing Node.js..."
 timeout -s SIGINT -k 2s 10s node loop_benchmark.js
-
-echo "Testing C#.NET 6.0 ..."
 timeout -s SIGINT -k 2s 10s LoopBenchmark/bin/Release/net6.0/linux-x64/publish/LoopBenchmark
-
-echo "Testing C#.NET 7.0 ..."
-timeout -s SIGINT -k 2s 10s LoopBenchmark/bin/Release/net7.0/linux-x64/publish/LoopBenchmark
-
-echo "Testing Java..."
 timeout -s SIGINT -k 2s 10s java LoopBenchmark
-
-echo "Testing Rust..."
 timeout -s SIGINT -k 2s 10s target/release/loop_benchmark_rust
-
-echo "Testing C++..."
 timeout -s SIGINT -k 2s 10s ./loop_benchmark_c++
 
